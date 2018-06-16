b3e488d7a62ee08a8fb36820ce725704da5c0e33
Kin Chan <xkinchanx@gmail.com>
2018-06-16 12:45:17 -0700

    [add] additional links for contact

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -4,8 +4,21 @@ import './Contact.css';
 
 const Contact = () =>
   <div id="contact">
-    <div className="contactButton">
-      <a href="mailto:xkinchanx@gmail.com"><div>Let's Connect!</div></a>
+    <div className="contactContainer">
+      <div className="contactHeader">
+        <h3>Let's Connect!</h3>
+      </div>
+      <div>
+        <div className="contactType">
+          <a href="mailto:xkinchanx@gmail.com">Contact</a>
+        </div>
+        <div className="contactType">
+          <a href="https://www.linkedin.com/in/thekinchan/" target="_blank">Linkedin</a>
+        </div>
+        <div className="contactType">
+          <a href="https://github.com/omegak911" target="_blank">Github</a> 
+        </div>
+      </div>
     </div>
   </div>
 
